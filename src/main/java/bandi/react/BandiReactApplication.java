97d8cfd1194e80b051684f97aadde6d63dc84afe
shinhyunho <youarefr@gmail.com>
2024-05-09 14:38:47 +0900

    Initial commit

---
+++ src/main/java/bandi/react/BandiReactApplication.java
@@ -0,0 +1,13 @@
+package bandi.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandiReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandiReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bandi/react/BandiReactApplicationTests.java
@@ -0,0 +1,13 @@
+package bandi.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandiReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
